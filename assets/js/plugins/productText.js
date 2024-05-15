19c5b983aa1b929be6f4190b5a980fd75441b6be
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-05-15 11:29:48 +0300

    commit

---
+++ assets/js/plugins/productText.js
@@ -0,0 +1,24 @@
+class ProductDetailsText {
+	constructor() {
+		this.container = document.querySelector('.product-details-desc');
+		this.content = this.container.querySelector('.product-details-desc__content');
+		this.text = this.container.querySelector('.product-details-desc__text');
+		this.button = this.container.querySelector('.product-details-desc__btn');
+
+		this._init();
+	}
+
+	_init() {
+		this.button.addEventListener('click', () => {
+			this.container.classList.toggle('_active');
+
+			if (this.container.classList.contains('_active')) {
+				this.content.style.height = this.text.scrollHeight + 50 + "px";
+				this.button.textContent = "Свернуть описание";
+			} else {
+				this.content.style.height = "100%";
+				this.button.textContent = "Полное описание";
+			}
+		});
+	}
+}
